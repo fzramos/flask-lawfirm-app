e7c32581ecd7413ab2cf331c08a8ab44009a9d21
fzramos <64625115+fzramos@users.noreply.github.com>
2020-11-06 15:08:42 -0600

    Adding Denise's Update, Delete

--- nza_app/routes.py
+++ nza_app/routes.py
@@ -4,13 +4,11 @@ from nza_app.forms import UserInfoForm, LoginForm, NoteForm
 from nza_app.models import User, Note, check_password_hash
 from flask_login import login_required,login_user, current_user, logout_user
 
-
-
-
 @app.route('/notes/create', methods = ['GET', 'POST'])
 @login_required
 def posts():
     form = NoteForm()
+
     if request.method == 'POST' and form.validate():
         case_name = form.case_name.data
         case_note = form.case_note.data
@@ -86,3 +84,31 @@ def register():
         
     return render_template('register.html',user_form = form)
 
+
+# Update Note Route -- 
+@app.route('/notes/update/<int:note_id>', methods = ['GET', 'POST'])
+@login_required
+def note_update(note_id): 
+    note = Note.query.get_or_404(note_id)
+    form = NoteForm()
+    if request.method == 'POST' and form.validate():
+        case_name = form.case_name.data
+        case_note = form.case_note.data
+        user_id = current_user.id
+        # Update the Database with the new Info 
+        note.case_name = case_name
+        note.case_note = case_note
+
+        # Commit the changes to the database
+        db.session.commit()
+        return redirect(url_for('show_notes'))
+    return render_template('note_update.html', update_form = form)
+
+# Delete Route 
+@app.route('/notes/delete/<int:note_id>', methods = ['GET', 'DELETE'])
+@login_required
+def note_delete(note_id): 
+    note = Note.query.get_or_404(note_id)
+    db.session.delete(note)
+    db.session.commit()
+    return redirect(url_for('show_notes'))
