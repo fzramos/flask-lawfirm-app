812ff597b81e515d649136f089ab7e8a7914046a
fearakil <apgomez15@gmail.com>
2020-11-06 17:46:01 -0600

    brought in original nza law html/css files, set up routes for html files, tried connecting respective css file to html file

---
+++ nza_app/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired,EqualTo, Email
+
--- nza_app/routes.py
+++ nza_app/routes.py
@@ -1,5 +1,17 @@
 from nza_app import app, db
-from flask import render_template,request
-from nza_app.forms import UserInfoForm, LoginForm
+from flask import render_template,request, redirect, url_for
+# from nza_app.forms import UserInfoForm, LoginForm
 from nza_app.models import User, Note, check_password_hash
 from flask_login import login_required,login_user, current_user, logout_user
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/what_we_do')
+def what_we_do():
+    return render_template('what.html')
